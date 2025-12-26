6490a7246e57fafc8183e904e793ff0dee0051d1
Serhio <biomugus@mail.ru>
2025-12-26 22:25:12 +0300

    Enhance Jest setup with cleanup after each test; update CSS media queries for responsive design across various components including ProductCard, Cta, Features, Footer, and Header, improving layout and styling for better user experience.

--- chupa_workshop/jest.setup.tsx
+++ chupa_workshop/jest.setup.tsx
@@ -1,8 +1,13 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { configure } from '@testing-library/react';
+import { configure, cleanup } from '@testing-library/react';
 
 configure({ asyncUtilTimeout: 3000 });
+
+afterEach(() => {
+  cleanup();
+  jest.clearAllMocks();
+})
 
 jest.mock('next/image', () => {
   const React = require('react');
